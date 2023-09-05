60687f6176868609e3c6acb421341b22fd3e6f7a
Pasindu Umayanga <pasinduumayanga93@gmail.com>
2023-09-05 18:41:28 +0530

    camera changes

--- src/CameraApp.tsx
+++ src/CameraApp.tsx
@@ -1,12 +1,63 @@
 import React, { LegacyRef, useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import RecordRTC from "recordrtc";
+import { json } from "stream/consumers";
 
 const CameraApp: React.FC = () => {
   const webcamRef = useRef<Webcam | null>(null);
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
   const [isRecording, setIsRecording] = useState(false);
   const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
+
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    // Add a window resize event listener
+    window.addEventListener("resize", handleResize);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  // useEffect(() => {
+  //   async function getCamera() {
+  //     try {
+  //       navigator?.mediaDevices?.getUserMedia({ video: true });
+  //       if (!navigator?.mediaDevices?.getUserMedia) {
+  //         alert(`Error accessing camera:undefined`);
+  //       }
+
+  //       if (navigator?.mediaDevices?.getUserMedia) {
+  //         const stream = await navigator.mediaDevices.getUserMedia({
+  //           video: true,
+  //           audio: true, // You can enable audio access if needed
+  //         });
+  //         if (webcamRef?.current?.video) {
+  //           webcamRef.current.video.srcObject = stream;
+  //         }
+  //       } else {
+  //         alert(
+  //           "getUserMedia is not supported in this browser. Please use a modern browser."
+  //         );
+  //       }
+  //     } catch (error) {
+  //       alert(`Error accessing camera: ${error}`);
+  //       console.error("Error accessing camera:", error);
+  //     }
+  //   }
+
+  //   getCamera();
+  // }, []);
 
   const handleStartRecording = () => {
     if (webcamRef.current) {
@@ -34,6 +85,9 @@ const CameraApp: React.FC = () => {
   const [capturedImage, setCapturedImage] = useState(null);
 
   const captureImage = () => {
+    if (webcamRef == null || webcamRef == undefined) {
+      alert("Devices not found");
+    }
     const imageSrc = webcamRef?.current?.getScreenshot();
     setCapturedImage(imageSrc ? imageSrc : ({} as any));
   };
@@ -50,15 +104,17 @@ const CameraApp: React.FC = () => {
 
   return (
     <div>
+      <video hidden id="videoElement" autoPlay playsInline></video>
       <h1>Webcam Video Recording</h1>
       <div>
         <Webcam
-          audio={true}
+          forceScreenshotSourceSize
+          audio={false}
           ref={webcamRef}
           videoConstraints={{
-            width: 1280,
-            height: 720,
-            facingMode: "user",
+            width: windowSize.width,
+            height: windowSize.height,
+            facingMode: "environment",
           }}
         />
       </div>
@@ -67,7 +123,12 @@ const CameraApp: React.FC = () => {
         {capturedImage && (
           <div>
             <h2>Captured Image:</h2>
-            <img src={capturedImage} alt="Captured" width={500} height={500} />
+            <img
+              src={capturedImage}
+              alt="Captured"
+              width={windowSize.width}
+              height={windowSize.height}
+            />
           </div>
         )}
         {isRecording ? (
