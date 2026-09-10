6efcef19bc6518e526c5dcf8f4580d7d746be302
PasinduUmayanga <pasinduumayanga93@gmail.com>
2026-09-10 12:56:25 +0530

    Add AppVeyor configuration, update dependencies, and enhance README with setup instructions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import WebcamCapture from "./WebcamCapture";
 import CameraApp from "./CameraApp";
 
 function App() {
   return (
     <div className="App">
-      {/* <WebcamCapture /> */}
       <CameraApp />
     </div>
   );
